5f63342fe244d4c110b4654a1bcdc015283eced8
Matthew Fenwick <952655+mattfenwick@users.noreply.github.com>
2022-10-19 11:09:10 -0400

    get up and running with skaffold

---
+++ cluster.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -xv
+set -euo pipefail
+
+KIND_NODE_IMAGE="kindest/node:v1.24.6"
+REGISTRY_PORT=5000
+REGISTRY_IMAGE="docker.io/library/registry:2"
+REGISTRY_NAME="kind-registry"
+
+
+kind create cluster --config=./dev/scripts/kind-config.yaml --image "$KIND_NODE_IMAGE"
+
+# Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+cat <<-EOF  | kubectl apply -f -
+    apiVersion: v1
+    kind: ConfigMap
+    metadata:
+      name: local-registry-hosting
+      namespace: kube-public
+    data:
+      localRegistryHosting.v1: |
+        host: "localhost:${REGISTRY_PORT}"
+        help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
+
+running="$(docker inspect -f '{{.State.Running}}' "${REGISTRY_NAME}" 2>/dev/null || true)"
+if [ "${running}" != 'true' ]; then
+  docker run \
+    --network "kind" \
+    -d --restart=always -p "127.0.0.1:${REGISTRY_PORT}:5000" --name "${REGISTRY_NAME}" \
+    "$REGISTRY_IMAGE"
+fi
---
+++ dev-up.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -xv
+set -euo pipefail
+
+CREATE_CLUSTER=${CREATE_CLUSTER:-false}
+
+if [[ $CREATE_CLUSTER == true ]]; then
+  ./cluster.sh
+fi
+
+TELEMETRY_NS=telemetry
+NGINX_NS="nginx"
+
+
+kubectl create ns $NGINX_NS || true
+kubectl create ns scaling || true
+kubectl create ns $TELEMETRY_NS || true
+
+kubectl create secret generic grafana-datasource \
+  --namespace "$TELEMETRY_NS" \
+  --from-file=grafana-datasource.yaml=./dev/scripts/grafana-datasource.yaml \
+  --dry-run=client \
+  -o yaml \
+  | kubectl apply -f -
+
+kubectl patch secret grafana-datasource \
+  --namespace "$TELEMETRY_NS" \
+  -p '{"metadata":{"labels":{"grafana_datasource": "1"}}}'
+
+# create dashboard configuration
+
+kubectl create secret generic grafana-dashboards \
+  --namespace "$TELEMETRY_NS" \
+  --from-file=./dev/scripts/grafana-dashboards \
+  --dry-run=client \
+  -o yaml \
+  | kubectl apply -f -
+
+kubectl patch secret grafana-dashboards \
+  --namespace "$TELEMETRY_NS" \
+  -p '{"metadata":{"labels":{"grafana_dashboard": "1"}}}'
+
+skaffold run
+
+#skaffold dev -m scaling
+#skaffold debug -m scaling
+
