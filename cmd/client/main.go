477bf68bd21f5fe5014495127cf16c8555bb0159
Matthew Fenwick <952655+mattfenwick@users.noreply.github.com>
2023-01-06 17:23:32 -0500

    simple create/fetch user example

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+	"context"
+	"os"
+
 	"github.com/mattfenwick/scaling/pkg/cli"
+	"github.com/mattfenwick/scaling/pkg/utils"
+	"github.com/mattfenwick/scaling/pkg/webserver"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	cli.Run()
+	isSimple := len(os.Args) < 2 || os.Args[1] != "false"
+	if isSimple {
+		myClient := webserver.NewClient("http://localhost:8765")
+		createResp, err := myClient.CreateUser(context.TODO(), &webserver.CreateUserRequest{Name: "abc", Email: "abc@def.org"})
+		utils.DoOrDie(err)
+		logrus.Infof("create response: %+v", createResp)
+
+		getResp, err := myClient.GetUser(context.TODO(), &webserver.GetUserRequest{UserId: createResp.UserId})
+		utils.DoOrDie(err)
+		logrus.Infof("get response: %+v", getResp)
+	} else {
+		cli.Run()
+	}
 }
